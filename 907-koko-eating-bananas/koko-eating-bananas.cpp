f04f3a3052f5157785e663c2d9da2c1b2b408a29
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-06-25 12:07:38 +0530

    Time: 15 ms (18.13%) | Memory: 23 MB (12.76%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -5,14 +5,14 @@ public:
         int l = 1;
         int r = *max_element(piles.begin(), piles.end());
         int ans = r;
-        while(l<= r){
+        while(l<=r){
             int mid = l + (r - l)/2;
             long long hour = 0;
             for(int num : piles){
                 hour += (num + mid - 1)/mid;
                 if(hour > h) break;
             }
-            if(hour <= h){
+            if(hour<= h){
                 ans = mid;
                 r = mid - 1;
             }
@@ -21,7 +21,5 @@ public:
             }
         }
         return ans;
-
-
     }
 };
