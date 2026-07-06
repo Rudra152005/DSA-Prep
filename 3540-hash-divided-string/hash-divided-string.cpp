70d7d1fe926ef1c1ab792595e266d2444a886775
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-07-06 23:16:39 +0530

    Time: 7 ms (11.85%) | Memory: 11.2 MB (37.93%) - LeetSync

---
+++ 3540-hash-divided-string/hash-divided-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string stringHash(string s, int k) {
+        string ans;
+
+        for (int i = 0; i < s.size(); i += k) {
+            int sum = 0;
+
+            for (int j = i; j < i + k; j++) {
+                sum += s[j] - 'a';
+            }
+
+            ans += char((sum % 26) + 'a');
+        }
+
+        return ans;
+    }
+};
