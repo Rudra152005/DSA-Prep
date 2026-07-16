533833ee3c2e42624c2072233717b970728489e2
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-07-16 10:16:35 +0530

    Time: 14 ms (22.93%) | Memory: 7.9 MB (38.50%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
+    int fun(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fun(n-1)+fun(n-2);
+    }
     int fib(int n) {
-        if(n <= 1) return n;
-
-        return fib(n - 1) + fib(n - 2);
-
+        return fun(n);
     }
 };
