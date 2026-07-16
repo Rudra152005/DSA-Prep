eaee641be81b138dbe041aef472dbd7569571385
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-07-16 12:18:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (23.64%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
+    int fun(vector<int> &nums,int i, vector<int> &dp){
+        if(i>=nums.size()) return 0;
+        if(dp[i] != -1) return dp[i];
+        int pick=nums[i]+fun(nums,i+2, dp);
+        int skip=fun(nums,i+1, dp);
+        return dp[i] = max(pick,skip);
+    }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1){
-            return nums[0];
-        }
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        for(int i=2; i<n; i++){
-            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-        return dp[n - 1];
+        vector<int> dp(nums.size() + 1, -1);
+        return max(fun(nums,0, dp),fun(nums,1, dp));
     }
 };
