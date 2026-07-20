c488dea629c8cab489a1419f0f3a19c9ae94e496
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-07-20 19:39:38 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (38.53%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,40 +8,32 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-    struct Compare {
-        bool operator()(ListNode* a, ListNode* b) {
-            return a->val > b->val;   // Min Heap
+    typedef ListNode* ln;
+    ListNode* mergetwo(ListNode* a, ListNode* b){
+        if(a == NULL) return b;
+        if(b == NULL) return a;
+        if(a->val <= b->val){
+            a->next = mergetwo(a->next, b);
+            return a;
         }
-    };
-
+        b->next = mergetwo(b->next, a);
+        return b;
+    }
+    ListNode* solve(vector<ListNode*> &lists, int l, int r){
+        if(l == r){
+            return lists[l];
+        }
+        int mid = l + (r - l)/2;
+        ln left = solve(lists, l, mid);
+        ln right = solve(lists, mid + 1, r);
+        return mergetwo(left, right);
+        
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0) return NULL;
 
-        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
-
-        // Push first node of every list
-        for (auto node : lists) {
-            if (node)
-                pq.push(node);
-        }
-
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
-
-        while (!pq.empty()) {
-
-            ListNode* curr = pq.top();
-            pq.pop();
-
-            tail->next = curr;
-            tail = tail->next;
-
-            if (curr->next)
-                pq.push(curr->next);
-        }
-
-        return dummy.next;
+        return solve(lists, 0, lists.size() - 1);
     }
 };
