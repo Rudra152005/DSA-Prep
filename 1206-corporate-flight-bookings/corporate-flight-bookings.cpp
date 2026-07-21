79b2de177abf1becb00e54124ac077876e7ee732
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-07-21 11:00:55 +0530

    Time: 4 ms (56.73%) | Memory: 72.1 MB (89.28%) - LeetSync

---
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> res(n);
+        for(int i=0; i<bookings.size(); i++){
+            int l = bookings[i][0];
+            int r = bookings[i][1];
+            int val = bookings[i][2];
+
+            res[l - 1] += val;
+            if(r < n) res[r] -= val;
+        }
+        for(int i = 1; i<res.size(); i++){
+            res[i] += res[i - 1];
+        }
+        return res;
+    }
+};
