022c56da3ea5e572e348c5030d817f3e6109f9e6
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-07-22 11:18:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (73.23%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -9,18 +9,18 @@ public:
         return dp[i][j]= right+down;
     }
     int tabulation(int m,int n){
-         vector<vector<int>> dp(m,vector<int>(n));
-        dp[0][0]=1;
+         vector<int> prev(n,1);
         for(int i=0;i<m;i++){
+            vector<int> curr(n);
             for(int j=0;j<n;j++){
-                if(i==0 && j==0) continue;
-                if(i==0 || j==0) dp[i][j]=1;
+                if(i==0 || j==0) curr[j]=1;
                 else{
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                    curr[j]=prev[j]+curr[j-1];
                 }
             }
+            prev=curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
     int uniquePaths(int m, int n) {
         // vector<vector<int>> dp(m,vector<int>(n,-1));
