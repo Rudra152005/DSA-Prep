def52bdaa35d1c965e18e428bd24403ded0a90d9
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-07-27 20:40:03 +0530

    Time: 6 ms (19.15%) | Memory: 18 MB (64.07%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -5,17 +5,14 @@ public:
         for(int num : nums){
             mp[num]++;
         }
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        for(auto it : mp){
+        priority_queue<pair<int, int>> pq;
+        for(auto it:mp){
             pq.push({it.second, it.first});
-            if(pq.size() > k){
-                pq.pop();
-            }
         }
         vector<int> ans;
-        while(!pq.empty()){
+        while(k != 0){
             ans.push_back(pq.top().second);
+            k--;
             pq.pop();
         }
         return ans;
