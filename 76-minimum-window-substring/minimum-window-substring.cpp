21845743a6fb759535c4484362a37b4ef683c0ef
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-07-27 23:54:44 +0530

    Time: 17 ms (26.98%) | Memory: 11.7 MB (15.91%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> need;
+        unordered_map<char, int> wind;
+        for(char c : t){
+            need[c]++;
+        }
+        int requ = need.size();
+        int form = 0;
+        int left = 0;
+        int start = 0;
+        int minlen = INT_MAX;
+        for(int i=0; i<s.size();  i++){
+            char c = s[i];
+            wind[c]++;
+            if(need.count(c) && wind[c] == need[c]) form++;
+            while(left <= i && form == requ){
+                if(i - left + 1 < minlen){
+                    minlen = i - left + 1;
+                    start = left;
+                }
+                char ch = s[left];
+                wind[ch]--;
+                if(need.count(ch) && wind[ch] < need[ch]) form--;
+                left++;
+            }
+        }
+        if(minlen == INT_MAX) return "";
+        return s.substr(start, minlen);
+    }
+};
