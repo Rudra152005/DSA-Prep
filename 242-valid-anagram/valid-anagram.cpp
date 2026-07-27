472b107138d63b55838ed36ba0ece3a220d66cf5
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-07-27 20:17:33 +0530

    Time: 1 ms (54.95%) | Memory: 9.7 MB (50.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()){
-            return false;
-        }
+        if(s.size() != t.size()) return false;
         unordered_map<char, int> mp;
-        for(char ch : s){
-            mp[ch]++;
+        for(char c : s){
+            mp[c]++;
         }
-        for(char ch : t){
-            mp[ch]--;
-            if(mp[ch] < 0){
+        for(char c : t){
+            mp[c]--;
+            if(mp[c] < 0){
                 return false;
             }
         }
-        
         return true;
     }
 };
