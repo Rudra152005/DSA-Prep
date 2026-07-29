ccf122fe021593db2b56b48132d9c07d125cebc5
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-07-29 11:02:19 +0530

    Time: 26 ms (22.62%) | Memory: 8.3 MB (11.09%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
+    int fibbo(int n, vector<int> &dp){
+        if(n == 0 || n == 1) return n;
+        if(dp[n] == -1) return dp[n];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[n] = fibbo(n - 2, dp) + fibbo(n - 1, dp);
+        return dp[n];
+    }
+
     int fib(int n) {
-        if(n == 0 || n== 1) return n;
-        return fib(n - 1) + fib(n - 2);
+        vector<int> dp(n  + 1);
+        return fibbo(n, dp);
     }
 };
