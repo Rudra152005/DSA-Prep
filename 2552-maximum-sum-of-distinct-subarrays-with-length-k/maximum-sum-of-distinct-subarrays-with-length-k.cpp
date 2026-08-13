c739096eef2dfcbd8d7131cc31a2dd30beec6769
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-08-13 16:54:04 +0530

    Time: 95 ms (83.93%) | Memory: 95.3 MB (47.76%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -8,23 +8,20 @@ public:
             mp[nums[i]]++;
         }
         long long maxsum = 0;
-        if(mp.size() == k){
-            maxsum = sum;
-        }
+        if(mp.size() == k) maxsum = sum;
         for(int i=k; i<nums.size(); i++){
             sum += nums[i];
             mp[nums[i]]++;
-            sum -= nums[i - k];
+            sum -= nums[i-k];
             mp[nums[i - k]]--;
             if(mp[nums[i - k]] == 0){
-                mp.erase(nums[i-k]);
+                mp.erase(nums[i - k]);
             }
             if(mp.size() == k){
                 maxsum = max(maxsum, sum);
             }
-
         }
         return maxsum;
-        
+
     }
 };
