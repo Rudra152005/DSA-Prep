96494a9fb31386a86ddde3457a08caef42b2780f
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-08-18 00:18:33 +0530

    Time: 15 ms (77.84%) | Memory: 19.5 MB (72.42%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,12 +1,3 @@
 class Solution:
     def canConstruct(self, ran: str, mag: str) -> bool:
-        mp = {}
-        for i in range(len(mag)):
-            mp[mag[i]] = mp.get(mag[i], 0) + 1
-        for ch in ran:
-            mp[ch] = mp.get(ch, 0) - 1
-            if mp[ch] < 0:
-                return False
-        return True
-
-        
+        return not Counter(ran) - Counter(mag)
