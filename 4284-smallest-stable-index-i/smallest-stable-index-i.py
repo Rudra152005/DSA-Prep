7a1fdb0ab3033f789356b3e5604c3ed48135543d
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-09-04 23:12:10 +0530

    Time: 4 ms (58.13%) | Memory: 19.3 MB (71.43%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.py
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -4,7 +4,7 @@ class Solution:
         maxlen = [0] * n
         minlen = [0] * n
         maxlen[0] = nums[0]
-        for i in range(len(nums)):
+        for i in range(1,len(nums)):
             maxlen[i] = max(maxlen[i - 1], nums[i])
         minlen[n - 1] = nums[n -1]
         for i in range(n-2, -1, -1):
