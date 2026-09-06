228f436c8609852a10ad82430c5d00ad3eab128b
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-09-06 18:16:40 +0530

    Time: 124 ms (31.64%) | Memory: 19.7 MB (73.98%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,43 @@
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not s or not t or len(s) < len(t):
+            return ""
+
+        ans = ""
+        need = Counter(t)
+        wind = {}
+
+        req = len(need)
+        form = 0
+
+        left = 0
+        min_len = float('inf')
+
+        for i in range(len(s)):
+            ch = s[i]
+
+            # Add current character to window
+            wind[ch] = wind.get(ch, 0) + 1
+
+            # Requirement for this character is satisfied
+            if ch in need and wind[ch] == need[ch]:
+                form += 1
+
+            # Try shrinking window
+            while form == req:
+
+                if i - left + 1 < min_len:
+                    ans = s[left:i + 1]
+                    min_len = i - left + 1
+
+                left_ch = s[left]
+
+                wind[left_ch] -= 1
+                if left_ch in need and wind[left_ch] < need[left_ch]:
+                    form -= 1
+
+                left += 1
+
+        return ans
