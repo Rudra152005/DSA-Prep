e1d7fd68164ada41c67a87be3f8489be29fb6cde
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-09-10 18:24:27 +0530

    Time: 3 ms (65.58%) | Memory: 21 MB (22.09%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -1,10 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         res = []
-        n = len(nums)
-        for i in range(len(nums)):
-            if nums.count(nums[i]) == 1:
-                res.append(nums[i])
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        for num in freq:
+            if freq[num] == 1:
+                res.append(num)
         return res
-
         
