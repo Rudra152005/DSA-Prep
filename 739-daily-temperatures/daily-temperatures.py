95f9d02af1ea6b3fd210caee9d6d6a737990e0a2
Rudra <148483382+Rudra152005@users.noreply.github.com>
2026-09-22 19:29:52 +0530

    Time: 123 ms (18.91%) | Memory: 28.8 MB (39.43%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,10 +1,13 @@
 class Solution:
-    def dailyTemperatures(self, temp: List[int]) -> List[int]:
+    def dailyTemperatures(self, temp: list[int]) -> list[int]:
         st = []
-        ans = [0]*len(temp)
+        ans = [0] * len(temp)
         for i in range(len(temp)):
             while len(st) > 0 and temp[st[-1]] < temp[i]:
                 ans[st[-1]] = i - st[-1]
                 st.pop()
             st.append(i)
         return ans
+
+
+        
